859fb630a1f19b4a80c1fd61280b655452030e56
Nathan Rymarz <nrymarz@gmail.com>
2021-09-30 10:22:39 -0500

    Hello world 5 times

--- App/Program.vb
+++ App/Program.vb
@@ -2,6 +2,8 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        For i=0 To 5
+            Console.WriteLine("Hello World!")
+        Next
     End Sub
 End Module
