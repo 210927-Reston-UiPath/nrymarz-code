02b34c98c401ce6f2cd5b7547ae46a98d6adcffc
Nathan Rymarz <nrymarz@gmail.com>
2021-09-30 11:06:30 -0500

    created menu

--- App/Program.vb
+++ App/Program.vb
@@ -2,8 +2,18 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        For i=0 To 5
-            Console.WriteLine("Hello World!")
-        Next
+        Do While True
+            Console.WriteLine("What would you like to do?")
+            Console.WriteLine("[0] Say Hello")
+            Console.WriteLine("[x] Exit")
+            Dim input as string = Console.ReadLine()
+            Select Case input
+                Case "0"
+                    Console.WriteLine("Hello")
+                Case "x"
+                    Console.WriteLine("Goodbye")
+                    Exit Do
+            End Select
+        Loop
     End Sub
 End Module
